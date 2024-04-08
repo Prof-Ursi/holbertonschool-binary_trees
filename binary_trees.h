45364d2936077ff526edf590c885db553d174a31
Prof-Ursi <8743@holbertonstudents.com>
2024-04-08 17:14:55 +0200

    Adding binary_trees.h to Repo

--- binary_trees.h
+++ binary_trees.h
@@ -14,14 +14,15 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s /*_s for "structure"*/
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t; /*_t for "type"*/
+};
 
+typedef struct binary_tree_s binary_tree_t; /*Binary Tree*/
 typedef struct binary_tree_s bst_t; /*Binary Search Tree*/
 typedef struct binary_tree_s avl_t; /*AVL Tree*/
 typedef struct binary_tree_s heap_t; /*Max Binary Heap*/
