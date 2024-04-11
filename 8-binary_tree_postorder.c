87053bfa1dc4a97df9e48971f4ec958dda76f7e3
nostop10 <angeberresafou@gmail.com>
2024-04-11 09:50:33 +0000

    8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,25 +1,11 @@
-#include <stdio.h>
 #include "binary_trees.h"
 
-void print_num(int n)
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    printf("%d\n", n);
-}
+    if (tree == NULL || func == NULL)
+        return;
 
-int main(void)
-{
-    binary_tree_t *root;
-
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    root->left->left = binary_tree_node(root->left, 6);
-    root->left->right = binary_tree_node(root->left, 56);
-    root->right->left = binary_tree_node(root->right, 256);
-    root->right->right = binary_tree_node(root->right, 512);
-
-    binary_tree_print(root);
-    binary_tree_postorder(root, &print_num);
-
-    return 0;
+    binary_tree_postorder(tree->left, func);
+    binary_tree_postorder(tree->right, func);
+    func(tree->n);
 }
